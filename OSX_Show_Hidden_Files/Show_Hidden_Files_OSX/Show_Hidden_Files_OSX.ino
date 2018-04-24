a2604d9dade806eade8285b526ab17cbb2b08879
John O'Neill <johnon@kainos.com>
2018-04-25 00:02:05 +0100

    Code optimisation

--- OSX_Show_Hidden_Files/Show_Hidden_Files_OSX/Show_Hidden_Files_OSX.ino
+++ OSX_Show_Hidden_Files/Show_Hidden_Files_OSX/Show_Hidden_Files_OSX.ino
@@ -1,41 +1,37 @@
 #include "Keyboard.h"
 #include "Mouse.h"
 
-int pause = 400;
+int pause = 250;
 
 void setup(){
-  Keyboard.begin();
-  Mouse.begin();
-  openSpotlight();
-  Keyboard.print("terminal");
-  delayAndEnter();
-  Keyboard.print("defaults write com.apple.finder AppleShowAllFiles true");
-  delayAndEnter();
-  killAndRestartFinder();
-  clearup();
-  Keyboard.end();
+    Keyboard.begin();
+    Mouse.begin();
+    delay(500);
+    openSpotlight();
+    input("terminal");
+    input("defaults write com.apple.finder AppleShowAllFiles true");
+    killAndRestartFinder();
+    clearup();
+    Keyboard.end();
+}
+
+void input(String cmd){
+    Keyboard.print(cmd);
+    delay(pause);
+    typeKey(KEY_RETURN);
+    delay(pause);
 }
 
 void typeKey(int key){
-   Keyboard.press(key);
-   delay(5);
-   Keyboard.release(key);
-}
-
-void delayAndEnter(){
-    delay(pause);
-    typeKey(KEY_RETURN);
-    delay(pause);
+    Keyboard.press(key);
+    delay(5);
+    Keyboard.release(key);
 }
 
 void killAndRestartFinder(){
-    Keyboard.print("killall Finder");
-    delay(pause);
-    typeKey(KEY_RETURN);
+    input("killall Finder");
     openSpotlight();
-    Keyboard.print("Finder");    
-    delay(pause);
-    typeKey(KEY_RETURN);
+    input("Finder");
 }
 
 void openSpotlight(){
@@ -48,18 +44,9 @@ void openSpotlight(){
 
 void clearup(){
     openSpotlight();
-    Keyboard.print("terminal");
-    delay(pause);
-    typeKey(KEY_RETURN);
-
-    delay(pause);
-    Keyboard.print("clear");
-    typeKey(KEY_RETURN);
-
-    delay(pause);
-    Keyboard.print("killall Terminal");
-    delay(pause);
-    typeKey(KEY_RETURN);
+    input("terminal");
+    input("clear");
+    input("killall Terminal");
 }
 
 void loop(){
